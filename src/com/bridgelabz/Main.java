e37be181dc17efe6b002c78a1154c36018af43c2
iaksharhate <iaksharhate@gmail.com>
2022-05-11 18:12:18 +0530

    Added: Case 1.

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int num = input.nextInt();
+
+        switch (num){
+            case 1:
+                System.out.println("One");
+            break;
+            default:
+                System.out.println("Invalid input.");
+        }
+    }
+}
